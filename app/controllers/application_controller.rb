2b4bcea04575fb8b7c79c37bd3d46d341d9d78aa
jessica you <jessicaxytan@gmail.com>
2021-09-09 17:29:35 -0400

    commit tree e89f3e910a0921c0a67a1550186dead817e91c24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,47 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+#   POST /baked_goods
+#   creates a new baked good in the database (FAILED - 1)
+#   returns data for the newly created baked good as JSON (FAILED - 2)
+
+  post '/baked_goods' do
+    # access data in body of request and create info
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    # send response with new record to JSON
+    baked_good.to_json
+  end
+
+
+# PATCH /bakeries/:id
+#   updates the name of the bakery in the database (FAILED - 3)
+#   returns data for the updated bakery as JSON (FAILED - 4)
+
+  patch '/bakeries/:id' do
+    # find the bakery by id
+    bakery = Bakery.find(params[:id])
+    # access and update in body
+    bakery.update(
+      name: params[:name]
+    )
+    # send to json
+    bakery.to_json
+  end
+
+
+# DELETE /baked_goods/:id
+#   deletes the baked good from the database (FAILED - 5)
+  delete '/baked_goods/:id' do
+    # find by id
+    baked_good = BakedGood.find(params[:id])
+    # destroy record
+    baked_good.destroy
+    # send to json
+    baked_good.to_json
+  end
+
 end
